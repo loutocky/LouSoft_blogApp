a9c721c760333352f66db2607b0da5c9c45bcd15
Jaroslav Loutocky <jaroslav.loutocky@intelstudios.com>
2023-01-24 16:34:27 +0100

    fix(ui): deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
-docker ps -q --filter ancestor="blogApp:v1" | xargs -r docker stop
+docker ps -q --filter ancestor="blog-app:v1" | xargs -r docker stop
 docker system prune --force
-docker build -t blogApp:v1 .
-docker run -d --name blogApp -it -p 6000:6000 blogApp:v1
+docker build -t blog-app:v1 .
+docker run -d --name blog-app -it -p 6000:6000 blog-app:v1
